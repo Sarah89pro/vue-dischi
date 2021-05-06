39cc20af416abae65504334485934d8b319557a9
Sarah89Pro <sarahproietti1@gmail.com>
2021-05-06 16:13:29 +0200

    init+header

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+        <div class="logo">
+        <img src="@/assets/img/logo.png" alt="logo">
+        </div>
+        
+    </header>
+  
+</template>
+
+<script>
+
+export default {
+    name: 'Header',
+};
+
+</script>
+
+<style scoped lang="scss">
+
+header {
+    display: flex;
+    justify-content: flex-start;
+    background-color: #2E3A46;
+    height: 65px;
+}
+
+.logo {
+    width: 45px;
+    margin: 10px;
+}
+
+.logo img {
+    max-width: 100%;
+    vertical-align: middle;
+}
+
+
+@import '@/styles/general';
+
+</style>
